641ff8b308a939341a5af1ed122dc1aecbe2fa17
pyke9 <tuanpyke99@gmail.com>
2025-03-08 00:16:58 +0700

    say Hi

---
+++ dang/8th-March/Scripts/homepage.js
@@ -0,0 +1,13 @@
+if (document.querySelector(".container").offsetWidth <= 500) {
+	document.querySelector(".background").src =
+		"./dang/8th-March/Images/background-phone.jpg";
+}
+function move() {
+	const input = document.querySelector(".input > input");
+	if (input.value != "") {
+		location.replace("./app/app.html");
+		localStorage.setItem("name", input.value);
+	} else {
+		alert("Nhập tên cái đi nè :3");
+	}
+}
